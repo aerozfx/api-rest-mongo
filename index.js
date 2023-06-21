24572c668212d4557d57a7ad27ce554f8ad9289c
aerozfx <aerozfx@gmail.com>
2023-06-21 22:33:05 +0200

    add '/' view

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.use(helmet());
 app.use("/api/products", productRouter);
 app.use("/api/providers", providersRouter);
 
+app.get("/", (req, res) => {
+  res.status(200).send(`<h2>Estos son algunos de los endpoints</h2>
+  <ul>
+  <li><a href="/api/products">/api/products</a></li>
+  <li><a href="/api/providers">/api/providers</a></li>
+  </ul>`);
+});
+
 app.listen(port, () => {
   console.info(`Servidor escuchando el puerto ${port}`);
 });
