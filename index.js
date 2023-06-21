c32f7b944ba8abf991149eba7f5a66114d7f33a7
aerozfx <aerozfx@gmail.com>
2023-06-21 22:28:43 +0200

    add helmet dependency

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const helmet = require("helmet");
 const app = express();
 const port = 3000;
 const productRouter = require("./routes/productsRouter");
 const providersRouter = require("./routes/providersRouter");
 
 app.use(express.json());
+app.use(helmet());
 
 app.use("/api/products", productRouter);
 app.use("/api/providers", providersRouter);
